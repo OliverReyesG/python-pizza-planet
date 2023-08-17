7775e16eb0bce6db5ded1c6be0ac9b7a53892c5d
oliverreyesg <ing.oliverreyesgutierrez@gmail.com>
2023-08-17 01:39:42 -0400

    feat: implement beverage service

--- app/repositories/serializers.py
+++ app/repositories/serializers.py
@@ -1,11 +1,19 @@
 from app.plugins import ma
-from .models import Ingredient, Size, Order, OrderDetail
+from .models import Ingredient, Size, Order, OrderDetail, Beverage
 
 
 class IngredientSerializer(ma.SQLAlchemyAutoSchema):
 
     class Meta:
         model = Ingredient
+        load_instance = True
+        fields = ('_id', 'name', 'price')
+
+
+class BeverageSerializer(ma.SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Beverage
         load_instance = True
         fields = ('_id', 'name', 'price')
 
@@ -27,7 +35,9 @@ class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
         load_instance = True
         fields = (
             'ingredient_price',
-            'ingredient'
+            'ingredient',
+            'beverage_price',
+            'beverage',
         )
 
 
