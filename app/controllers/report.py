2b9f7d43bfad6431bd15c1dfde35d7a01d7f8e9c
oliverreyesg <ing.oliverreyesgutierrez@gmail.com>
2023-08-19 12:49:03 -0400

    refactor: report format

--- app/controllers/report.py
+++ app/controllers/report.py
@@ -7,12 +7,37 @@ class ReportController:
     session = db.session
 
     @classmethod
+    def _get_top_ingredient(cls, serialized_ingredients: list):
+        top_ingredient = serialized_ingredients[0]
+        for ingredient in serialized_ingredients:
+            if ingredient.get("requests") > top_ingredient.get("requests"):
+                top_ingredient = ingredient
+        return top_ingredient
+
+    @classmethod
+    def _get_top_month(cls, serialized_months: list):
+        top_month = serialized_months[0]
+        for month in serialized_months:
+            if month.get("revenue") > top_month.get("revenue"):
+                top_month = month
+        return top_month
+
+    @classmethod
+    def _get_top_customer(cls, serialized_customers: list):
+        sorted_customers = sorted(
+            serialized_customers, key=lambda customer: customer.get("revenue"), reverse=True)
+        return sorted_customers[:3]
+
+    @classmethod
     def get_most_requested_ingredient(cls):
         try:
             response = cls.session.query(Ingredient, func.count(OrderDetail._id).label("requests")).join(
-                OrderDetail).group_by(Ingredient._id).order_by(desc("requests")).all()
-            serialized_response = [
-                {"ingredient_id": row[0]._id, "ingredient_name": row[0].name, "requests": row[1]} for row in response]
+                OrderDetail).group_by(Ingredient._id).all()
+            serialized_response = {"ingredients": [
+                {"ingredient_id": row[0]._id, "ingredient_name": row[0].name, "requests": row[1]} for row in response]}
+            top_ingredient = cls._get_top_ingredient(
+                serialized_ingredients=serialized_response.get("ingredients"))
+            serialized_response["top_ingredient"] = top_ingredient
             return serialized_response, None
         except Exception as e:
             return None, e
@@ -25,8 +50,10 @@ class ReportController:
                 func.extract('month', Order.date).label('month'),
                 func.sum(Order.total_price).label('record_count')
             ).group_by('year', 'month').all()
-            serialized_response = [
-                {"year": row[0], "month": row[1], "revenue": round(row[2], 2)} for row in response]
+            serialized_response = {"months": [
+                {"year": row[0], "month": row[1], "revenue": round(row[2], 2)} for row in response]}
+            top_month = cls._get_top_month(serialized_months=serialized_response.get("months"))
+            serialized_response["top_month"] = top_month
             return serialized_response, None
         except Exception as e:
             return None, e
@@ -36,8 +63,11 @@ class ReportController:
         try:
             response = cls.session.query(Order.client_name, func.sum(
                 Order.total_price).label('revenue')).group_by(Order.client_name, Order.client_dni).order_by(desc('revenue')).all()
-            serialized_response = [
-                {"client_name": row[0], "revenue": round(row[1], 2)} for row in response]
+            serialized_response = {"customers": [
+                {"client_name": row[0], "revenue": round(row[1], 2)} for row in response]}
+            top_customers = cls._get_top_customer(
+                serialized_customers=serialized_response.get("customers"))
+            serialized_response["top_customers"] = top_customers
             return serialized_response, None
         except Exception as e:
             return None, e
@@ -50,7 +80,8 @@ class ReportController:
             revenue_by_customer = cls.get_revenue_by_customer()[0]
 
             serialized_response = {"ingredient_report": ingredient_report,
-                                   "revenue_by_month": revenue_by_month, "revenue_by_customer": revenue_by_customer}
+                                   "revenue_by_month": revenue_by_month,
+                                   "revenue_by_customer": revenue_by_customer}
             return serialized_response, None
         except Exception as e:
             return None, e
